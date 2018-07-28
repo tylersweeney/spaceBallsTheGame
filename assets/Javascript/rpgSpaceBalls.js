3383e9e454a28f6a35cc8d23275c95622097d496
Tyler Sweeney <tylersweeney@gmail.com>
2018-07-28 14:35:53 -0700

    add character objects to js file

---
+++ assets/Javascript/rpgSpaceBalls.js
@@ -0,0 +1,34 @@
+// Execute this code when the DOM has fully loaded.
+
+$(document).ready(function(){
+    var characters = {
+        "Lone Starr": {
+            name: "Lone Starr",
+            health: 120,
+            attack: 8,
+            imageUrl: "assets/Images/loneStarr.gif",
+            enemyAttackBack: 15
+        },
+        "Dark Helmet" : {
+            name: "Dark Helmet",
+            health: 100,
+            attack: 14,
+            imageUrl: "assets/Images/darkHelmet.gif",
+            enemyAttackBack: 5
+        },
+        "Barf" : {
+            name: "Barf",
+            health: 150,
+            attack: 8,
+            imageURL: "assets/Images/barf.gif",
+            enemyAttackBack: 5
+        },
+        "President Skroob" : {
+            name: "President Skroob",
+            health: 180,
+            attack: 7,
+            imageUrl: "assets/Images/skroob.gif",
+            enemyAttackBack: 20
+        }
+    };
+});
