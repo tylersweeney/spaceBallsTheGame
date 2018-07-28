74b8acbf6690d38e59e17536d3f8ee7359f07ff3
Tyler Sweeney <tylersweeney@gmail.com>
2018-07-28 15:09:47 -0700

    create jQuery render functions for characters

--- assets/Javascript/rpgSpaceBalls.js
+++ assets/Javascript/rpgSpaceBalls.js
@@ -31,4 +31,28 @@ $(document).ready(function(){
             enemyAttackBack: 20
         }
     };
+    console.log(characters);
+    // This function will render a character card to the page.
+    // The character rendered and the area they are rendered to
+    var renderOne = function(character, renderArea) {
+        var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+        var charName = $("<div class='character-name'>").text(character.name);
+        var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+        var charHealth = $("<div class='character-health'>").text(character.health);
+        charDiv.append(charName).append(charImage).append(charHealth);
+        $(renderArea).append(charDiv);
+    }
+    //This function handles the rendering of characters base on which are selected
+    var renderCharacters = function(charObj, areaRender) {
+        if (areaRender === "#characters-section") {
+            $(areaRender).empty();
+            for (var key in charObj) {
+                if(charObj.hasOwnProperty(key)) {
+                    renderOne(charObj[key], areaRender);
+                }
+            }
+        }
+    }
+    // Render all characters to the page when the game starts.
+    renderCharacters(characters, "#characters-section");
 });
