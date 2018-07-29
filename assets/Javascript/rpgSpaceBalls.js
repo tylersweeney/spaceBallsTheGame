d7dc190c9830adcbcd603a40d50b08b0663393c5
Tyler Sweeney <tylersweeney@gmail.com>
2018-07-28 18:32:54 -0700

    create logic for character selection

--- assets/Javascript/rpgSpaceBalls.js
+++ assets/Javascript/rpgSpaceBalls.js
@@ -32,28 +32,78 @@ $(document).ready(function(){
             enemyAttackBack: 20
         }
     };
-    console.log(characters);
+    var currSelectedCharacter;
+    var combatants = [];
+    // FUNCTIONS
+    // ================================================================================================
+
     // This function will render a character card to the page.
     // The character rendered and the area they are rendered to
-    var renderOne = function(character, renderArea) {
+    var renderOne = function(character, renderArea, charStatus) {
         var charDiv = $("<div class='character' data-name='" + character.name + "'>");
         var charName = $("<div class='character-name'>").text(character.name);
         var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
         var charHealth = $("<div class='character-health'>").text(character.health);
         charDiv.append(charName).append(charImage).append(charHealth);
         $(renderArea).append(charDiv);
+
+        if (charStatus === "enemy"){
+            $(charDiv).addClass("enemy");
+        }
     }
     //This function handles the rendering of characters base on which are selected
     var renderCharacters = function(charObj, areaRender) {
         if (areaRender === "#characters-section") {
             $(areaRender).empty();
+            //Loop through the characters object
             for (var key in charObj) {
                 if(charObj.hasOwnProperty(key)) {
-                    renderOne(charObj[key], areaRender);
+                    renderOne(charObj[key], areaRender, "");
                 }
             }
+        }
+
+        // "selected-character" is the div where our selected character
+        // If true, render the selected player character to this area
+        if (areaRender === "#selected-character"){
+            renderOne(charObj, areaRender, "");
+        }
+
+        // "available-to-attack" is the div where inactive opponenets are
+        if (areaRender === "#available-to-attack-section"){
+            for(var i = 0; i < charObj.length; i++) {
+                renderOne(charObj[i], areaRender, "enemy");
+            }
+
+            $(document).on("click", ".enemy", function(){
+                var name = ($(this).attr("data-name"));
+            })
         }
     }
     // Render all characters to the page when the game starts.
     renderCharacters(characters, "#characters-section");
+
+    // On click event for selecting our character.
+    $(document).on("click", ".character", function(){
+        // Saving the clicked character's name.
+        var name = $(this).attr("data-name");
+
+        if (!currSelectedCharacter) {
+            currSelectedCharacter = characters[name];
+            for(var key in characters) {
+                if(key!== name) {
+                    combatants.push(characters[key]);
+                }
+            }
+
+            console.log(combatants);
+            // Hide the character select div.
+            $("#characters-section").hide();
+
+            // Then render our selected character and our combatants.
+            renderCharacters(currSelectedCharacter, "#selected-character");
+            renderCharacters(combatants, "#available-to-attack-section");
+
+        }
+    })
 });
